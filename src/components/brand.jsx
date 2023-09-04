b0b3a4b0ab2a4df4c44cc8018b4bf155f91dff25
ajhits <ajderoque@gmail.com>
2023-09-04 17:18:56 +0800

    hello

--- src/components/brand.jsx
+++ src/components/brand.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export const Brand = (props) => {
+  // State to control the current slide
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const handlePrevClick = () => {
+    // Decrease the current slide index
+    setCurrentSlide((prevSlide) => prevSlide - 1);
+  };
+
+  const handleNextClick = () => {
+    // Increase the current slide index
+    setCurrentSlide((prevSlide) => prevSlide + 1);
+  };
+
   return (
     <div id="brand" className="text-center">
       <div className="container">
@@ -8,16 +23,39 @@ export const Brand = (props) => {
           <h2>Brands</h2>
         </div>
         <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
+          {props.data ? (
+            <Carousel
+              showArrows={false} // Hide the default arrows
+              autoPlay={true}
+              interval={3000}
+              showThumbs={false}
+              showStatus={false}
+              infiniteLoop={true}
+              selectedItem={currentSlide} // Control the current slide
+              renderArrowPrev={(onClickHandler, hasPrev) =>
+                hasPrev && (
+                  <button onClick={onClickHandler} className="custom-arrow custom-arrow-prev">
+                    Previous
+                  </button>
+                )
+              }
+              renderArrowNext={(onClickHandler, hasNext) =>
+                hasNext && (
+                  <button onClick={onClickHandler} className="custom-arrow custom-arrow-next">
+                    Next
+                  </button>
+                )
+              }
+            >
+              {props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`}>
+                  <img src={d.image} alt={d.title} style={{ width: '300px', height: '300px' }} />
                 </div>
-              ))
-            : "Loading..."}
+              ))}
+            </Carousel>
+          ) : (
+            "Loading..."
+          )}
         </div>
       </div>
     </div>
--- src/src/components/accountBox/accountContext.js
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from 'react';
-
-const AccountContext = createContext();
-
-export {AccountContext};
