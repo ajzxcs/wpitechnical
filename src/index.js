15f107609c35cafa53066405cc3b0bfdb31ef520
ajhits <ajderoque@gmail.com>
2023-09-08 09:20:11 +0800

    hshs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>    
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+// Material Dashboard 2 React Context Provider
+import { MaterialUIControllerProvider } from "context";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </MaterialUIControllerProvider>
+  </BrowserRouter>
 );
-
-
