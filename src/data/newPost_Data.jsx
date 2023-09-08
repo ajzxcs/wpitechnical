634d27320e820fb41826a34ea062e3cb1d2d1fbe
unknown <ogm.wd.intern4@gmail.com>
2023-09-08 13:43:01 +0800

    updated

---
+++ src/data/newPost_Data.jsx
@@ -0,0 +1,184 @@
+const POST = [
+    {
+        // Unique ID of the users
+        "1": {
+            "Author": "email@gmail.com",
+      
+            "Posts": [
+              {
+                "title": "How to Troubleshoot a Defibrillator",
+                "content": "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+                "date": "2023-08-27",
+                "comments": [
+                  {
+                    "id": 1,
+                    "author": "haha@gmail.com",
+                    "text": "Great guide! It helped me quickly diagnose an issue with our defibrillator.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 2,
+                    "author": "haha@gmail.com",
+                    "text": "I am enchanted to meet you",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 3,
+                    "author": "haha@gmail.com",
+                    "text": "Thanks for sharing! I had no idea how to troubleshoot a defibrillator before.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  }
+                ],
+      
+                "likes": 2,
+                "dislikes": 0
+              },
+              {
+                "title": "How to Troubleshoot a Defibrillator",
+                "content": "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+                "date": "2023-08-27",
+                "comments": [
+                  {
+                    "id": 1,
+                    "author": "haha@gmail.com",
+                    "text": "Great guide! It helped me quickly diagnose an issue with our defibrillator.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 2,
+                    "author": "haha@gmail.com",
+                    "text": "I am enchanted to meet you",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 3,
+                    "author": "haha@gmail.com",
+                    "text": "musika",
+                    "date and time" :"2023-08-27 4:00am" 
+                  }
+                ],
+                "likes": 5,
+                "dislikes": 0
+              }
+            ]
+          },
+      
+          "2": {
+            "Author": "authornumber2@gmail.com",
+            "Posts": [
+              {
+                "title": "How to Troubleshoot a Defibrillator",
+                "content": "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+                "date": "2023-08-27",
+                "comments": [
+                  {
+                    "id": 1,
+                    "author": "haha@gmail.com",
+                    "text": "Great guide! It helped me quickly diagnose an issue with our defibrillator.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 2,
+                    "author": "haha@gmail.com",
+                    "text": "I am enchanted to meet you"
+                  },
+                  {
+                    "id": 3,
+                    "author": "haha@gmail.com",
+                    "text": "Thanks for sharing! I had no idea how to troubleshoot a defibrillator before.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  }
+                ],
+                "likes": 2,
+                "dislikes": 0
+              },
+              {
+                "title": "How to Troubleshoot a Defibrillator",
+                "content": "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+                "date": "2023-08-27",
+                "comments": [
+                  {
+                    "id": 1,
+                    "author": "haha@gmail.com",
+                    "text": "Great guide! It helped me quickly diagnose an issue with our defibrillator."
+                  },
+                  {
+                    "id": 2,
+                    "author": "haha@gmail.com",
+                    "text": "I am enchanted to meet you"
+                  },
+                  {
+                    "id": 3,
+                    "author": "haha@gmail.com",
+                    "text": "musika"
+                  }
+                ],
+                "likes": 5,
+                "dislikes": 0
+              }
+            ]
+          },
+      
+          "3": {
+            "Author": "AnesthesiaPro@gmail.com",
+            "Posts": [
+              {
+                "title": "Guide to Maintaining Anesthesia Machines",
+                "content": "Anesthesia machines are critical in surgeries. Discover how to clean and maintain these machines for patient safety.",
+                "date": "2023-10-07",
+                "comments": [
+                  {
+                    "id": 1,
+                    "author": "haha@gmail.com",
+                    "text": "Your guide on maintaining anesthesia machines is comprehensive and informative. Thanks for sharing!"
+                  },
+                  {
+                    "id": 2,
+                    "author": "haha@gmail.com",
+                    "text": "I had some doubts about maintenance, but your guide cleared them up. Great work!",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 3,
+                    "author": "haha@gmail.com",
+                    "text": "Thanks for sharing! I had no idea how to troubleshoot a defibrillator before.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  }
+                ],
+                "likes": 2,
+                "dislikes": 0
+              },
+              {
+                "title": "How to Troubleshoot a Defibrillator",
+                "content": "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+                "date": "2023-08-27",
+                "comments": [
+                  {
+                    "id": 1,
+                    "author": "haha@gmail.com",
+                    "text": "Great guide! It helped me quickly diagnose an issue with our defibrillator.",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 2,
+                    "author": "haha@gmail.com",
+                    "text": "I am enchanted to meet you",
+                    "date and time" :"2023-08-27 4:00am" 
+                  },
+                  {
+                    "id": 3,
+                    "author": "haha@gmail.com",
+                    "text": "musika",
+                    "date and time" :"2023-08-27 4:00am" 
+                  }
+                ],
+                "likes": 5,
+                "dislikes": 0
+              }
+            ]
+          }
+        }
+];
+
+
+export default POST;
