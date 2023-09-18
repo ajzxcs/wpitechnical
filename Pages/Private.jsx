4a138153c22ba7791396b42aa84e6062e94269c0
WDFrancisBryan <143153810+WDFrancisBryan@users.noreply.github.com>
2023-09-18 15:28:37 +0800

    Add files via upload

---
+++ Pages/Private.jsx
@@ -0,0 +1,171 @@
+
+import React, { useState, useEffect } from "react";
+import Header from '../components/componentsPV/Headerpv';
+
+
+import { LogoutSession,userCredentials } from '../Features/Authentication/Authentication'
+import { viewList,createPost,addComments } from '../Features/firebase/Database'
+
+import "../App.css";
+
+
+
+function Public() {
+
+  // data from firebase
+  const [showDats,setShowDats] = useState([])
+
+  useEffect(() => {
+
+    let isMounted = true; // Flag to track whether the component is mounted
+
+
+    // update the data from firebase
+    const fetchData = async () => {
+      try {
+        const postsData = await viewList();
+   
+        setShowDats(postsData);
+      } catch (error) {
+        // Handle the error
+        console.log(error)
+      }
+    }
+
+    // Check if the component is still mounted before updating the state
+    if (isMounted) {
+      fetchData();
+    }
+
+
+    return () => {
+      isMounted = false; // Set the flag to false when the component unmounts
+    };
+  });
+
+
+
+  return (
+
+    <div className="public"style={{ display: 'flex', flexDirection: 'column', marginTop: "20px", justifyContent: 'center', alignItems: 'center' }} >
+      <Header />
+     
+
+    
+        <h1>PRIVATE FORUM</h1> 
+
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+
+
+        <div style={{ padding: '5px' }}> 
+        <button onClick={(event)=>
+        {
+          event.preventDefault();
+          LogoutSession();
+        }
+        }>LOG OUT</button>
+        </div>
+
+<div style={{ padding: '5px' }}> 
+      <button onClick={(event)=>
+      {
+        event.preventDefault();
+        userCredentials()
+        .then(e=>alert("User uniqueID: " + String(e.uniqueID) ))
+        .catch(e=>console.log(e))           
+        // console.log(userCredentials())                                                                            
+      }
+      }>Click me</button>
+      </div>
+
+{/* add post button */}
+      <div style={{ padding: '5px' }}> 
+        <button onClick={
+        event=>{
+          event.preventDefault();
+          // parameters data: Title, Content, Tags
+          createPost("no changes", "this is my second post", "A,B,C")
+            .then(e=>alert(e))
+            .catch(e=>alert(e))}
+        }>
+        add post
+        </button>
+      </div>
+    </div>
+
+
+   {/* Iterate of user posts */}
+      {showDats?.map((item, index) => (
+        <div key={index} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}} >
+         
+          {/* Iterate through the each Posts of user */}
+          {item.Posts && Object.values(item.Posts)?.map((post, postIndex) => (
+
+            <div key={postIndex}>
+
+              <h3>Title: {post.Title}</h3>
+
+              <p>Author: {item.author}</p>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>   
+
+                <div style={{padding: "5px"}}> 
+                  <p>Date: {post.date[0]} </p>
+                </div>
+
+              <div style={{padding: "5px"}}> 
+                <p>Time: {post.date[1]}</p> 
+
+                </div>
+              </div>
+
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}> 
+                <p><strong>TAGS: </strong></p>
+                <p>{post.tags}</p>
+              </div>
+
+              <p><strong>Content:</strong> {post.Content}</p>
+
+              <p>Likes: {post.likes}</p>
+              <p>Dislikes: {post.dislikes}</p>
+
+              {/* Render other properties as needed */}
+              <h4>Comments:</h4>
+
+           
+              <button key={postIndex} onClick={e=>{
+                e.preventDefault();
+                // const post = item.Posts[postId]; // Get the post data
+                addComments(post.id,item.author, "WOW THAT IS AWESOME")
+                console.log(String(post.id),String(item.author),"WOW that is awwesome")
+              }}>add comments</button>
+
+              {/* Iterate of all Post List */}
+              {post.Comments && Object.values(post.Comments)?.map((comment,index)=>(
+                <div key={index}>
+                  <h3>{comment.Text}</h3>
+                  <p><strong>{comment.Author}</strong></p>
+                  <p>Date: {comment.date[0]} </p>
+                  <p>Time: {comment.date[1]} </p>
+
+                </div>
+          
+              ))}
+              <hr/>
+             
+            </div>
+
+         
+
+          ))}
+
+
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+
+export default Public;
+
---
+++ data/samplePosts.jsx
@@ -0,0 +1,119 @@
+const samplePosts = {
+  "1": {
+    Author: "email@gmail.com",
+    Posts: [
+      {
+        title: "How to Troubleshoot a Defibrillator",
+        content:
+          "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+        date: "2023-08-27",
+        time: "4:00am",
+        Author: "email@gmail.com",
+        tags: ["Medical Devices", "Emergency", "Troubleshooting"],
+        comments: [
+          {
+            id: 1,
+            author: "haha@gmail.com",
+            text:
+              "Great guide! It helped me quickly diagnose an issue with our defibrillator.",
+            "date and time": "2023-08-27 4:00am"
+          },
+          {
+            id: 2,
+            author: "haha@gmail.com",
+            text: "I am enchanted to meet you"
+          },
+          {
+            id: 3,
+            author: "haha@gmail.com",
+            text:
+              "Thanks for sharing! I had no idea how to troubleshoot a defibrillator before.",
+            "date and time": "2023-08-27 4:00am"
+          }
+        ],
+        likes: 2,
+        dislikes: 0
+      }
+      // Add more posts for author 1 with tags here...
+    ]
+  },
+  "2": {
+    Author: "authornumber2@gmail.com",
+    Posts: [
+      {
+        title: "How to Troubleshoot a Defibrillator",
+        content:
+          "A defibrillator is crucial in emergencies. Learn how to troubleshoot common issues and ensure its proper functioning.",
+        date: "2023-07-27",
+        time: "4:00am",
+        Author: "authornumber2@gmail.com",
+        tags: ["Medical Devices", "Emergency", "Troubleshooting"],
+        comments: [
+          {
+            id: 1,
+            author: "haha@gmail.com",
+            text:
+              "Great guide! It helped me quickly diagnose an issue with our defibrillator.",
+            "date and time": "2023-08-27 4:00am"
+          },
+          {
+            id: 2,
+            author: "haha@gmail.com",
+            text: "I am enchanted to meet you"
+          },
+          {
+            id: 3,
+            author: "haha@gmail.com",
+            text:
+              "Thanks for sharing! I had no idea how to troubleshoot a defibrillator before.",
+            "date and time": "2023-08-27 4:00am"
+          }
+        ],
+        likes: 2,
+        dislikes: 0
+      }
+      // Add more posts for author 2 with tags here...
+    ]
+  },
+  "3": {
+    Author: "AnesthesiaPro@gmail.com",
+    Posts: [
+      {
+        title: "Guide to Maintaining Anesthesia Machines",
+        content:
+          "Anesthesia machines are critical in surgeries. Discover how to clean and maintain these machines for patient safety.",
+        date: "2023-06-07",
+        time: "6:06pm",
+        Author: "AnesthesiaPro@gmail.com",
+        tags: ["Medical Devices", "Surgery", "Maintenance"],
+        comments: [
+          {
+            id: 1,
+            author: "haha@gmail.com",
+            text:
+              "Your guide on maintaining anesthesia machines is comprehensive and informative. Thanks for sharing!"
+          },
+          {
+            id: 2,
+            author: "haha@gmail.com",
+            text:
+              "I had some doubts about maintenance, but your guide cleared them up. Great work!",
+            "date and time": "2023-10-07 4:00am"
+          },
+          {
+            id: 3,
+            author: "haha@gmail.com",
+            text:
+              "Thanks for sharing! I had no idea how to troubleshoot a defibrillator before.",
+            "date and time": "2023-10-07 4:00am"
+          }
+        ],
+        likes: 2,
+        dislikes: 0
+      }
+      // Add more posts for author 3 with tags here...
+    ]
+  }
+};
+
+export default samplePosts;
