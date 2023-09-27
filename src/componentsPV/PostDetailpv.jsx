5099f5e9ef09f835d587126ed0617211a4589e77
WDFrancisBryan <143153810+WDFrancisBryan@users.noreply.github.com>
2023-09-27 22:06:54 +0800

    Update PostDetailpv.jsx

--- src/componentsPV/PostDetailpv.jsx
+++ src/componentsPV/PostDetailpv.jsx
@@ -1,80 +1,74 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import "../assets/PostDetail.css";
 import "../assets/public.css";
-
-// database
-import { deletePost,addComment } from '../Features/firebase/Database'
-
+import { deletePost, addComment } from "../Features/firebase/Database";
 
 const Comment = ({ comment }) => (
   <div className="comment">
     <div className="comment-header">
       <p className="comment-author">{comment.Author}</p>
-      <p className="comment-date">{String(comment.date[0]) + " " + String(comment.date[1])}</p>
+      <p className="comment-date">
+        {String(comment.date[0]) + " " + String(comment.date[1])}
+      </p>
     </div>
     <p className="comment-text">{comment.Text}</p>
- 
   </div>
 );
 
 const PostDetail = ({ post, onGoBack }) => {
-  const [commentText, setCommentText] = React.useState("");
-  // const [comments, setComments] = React.useState(post.comments); // Use post.comments
+  const [commentText, setCommentText] = useState("");
+  const [isCommentEmpty, setIsCommentEmpty] = useState(false);
 
   const handleDeletePost = () => {
-   
-    deletePost(post.id)
+    deletePost(post.id);
   };
+
   const handleAddComment = () => {
-    // if (commentText.trim() !== "") {
-    //   const newComment = {
-    //     id: comments.length + 1,
-    //     text: commentText,
-    //     likes: 0,
-    //     dislikes: 0
-    //   };
-
-
-
-      addComment(post.id,post.Author,commentText)
-      .then(e=>{
-        alert(e)
-        window.location.reload()
-      })
-      .catch(e=>alert(e))
-
-      // setComments([...comments, newComment]);
-      // setCommentText("");
-    // }
+    if (commentText.trim() !== "") {
+      addComment(post.id, post.Author, commentText)
+        .then((e) => {
+          alert(e);
+          window.location.reload();
+        })
+        .catch((e) => alert(e));
+      setCommentText("");
+      setIsCommentEmpty(false);
+    } else {
+      // Set a flag to indicate the comment is empty and needs to be highlighted
+      setIsCommentEmpty(true);
+      // Add a CSS class to trigger the color animation
+      setTimeout(() => {
+        setIsCommentEmpty(false); // Reset the flag after the animation completes
+      }, 500); // Adjust the duration to match your CSS animation
+    }
   };
 
   return (
     <div className="post-detail">
-
-    {/* Back button */}
+      {/* Back button */}
       <div className="back-button">
         <button onClick={onGoBack}>
           <FontAwesomeIcon icon={faArrowLeft} /> Back
         </button>
       </div>
 
-
       <div className="title-and-delete">
-
-      {/* Title */}
+        {/* Title */}
         <h2>{post ? post.Title : "Post Not Found"}</h2>
 
-      {/* Delete */}
+        {/* Delete */}
         <button onClick={handleDeletePost}>
           <FontAwesomeIcon icon={faTrash} />
         </button>
       </div>
 
       {/* Author, date and time */}
-      <p>By <strong>{post?.Author}</strong> - {post.date && post.date[0]}, {post.date && post.date[1]}</p>
+      <p>
+        By <strong>{post?.Author}</strong> - {post.date && post.date[0]},{" "}
+        {post.date && post.date[1]}
+      </p>
 
       <br />
 
@@ -83,31 +77,31 @@ const PostDetail = ({ post, onGoBack }) => {
 
       {/* Comment Section */}
       <div className="comment-section">
-
         {/* List of Comments */}
         <h3>Comments:</h3>
-        {post && post.Comments ? 
-          (<div className="comment-list">
-            {Object.values(post.Comments)?.map((comment,index) => (
-              <Comment
-              key={index}
-              comment={comment}
-              />
+        {post && post.Comments ? (
+          <div className="comment-list">
+            {Object.values(post.Comments)?.map((comment, index) => (
+              <Comment key={index} comment={comment} />
             ))}
-          </div>) : ( <p>No comments available.</p> )}
-          
+          </div>
+        ) : (
+          <p>No comments available.</p>
+        )}
+
         {/* Comment section */}
         <textarea
-        rows="4"
-        value={commentText}
-        onChange={(e) => setCommentText(e.target.value)}
-        placeholder="Add a comment..."
+          rows="4"
+          value={commentText}
+          onChange={(e) => setCommentText(e.target.value)}
+          placeholder="Add a comment..."
+          className={isCommentEmpty ? "error" : ""}
         />
-  
-          <button className="comment-button" onClick={handleAddComment}>
-            Add Comment
-          </button>
-        </div>
+
+        <button className="comment-button" onClick={handleAddComment}>
+          Add Comment
+        </button>
+      </div>
 
       <br />
     </div>
