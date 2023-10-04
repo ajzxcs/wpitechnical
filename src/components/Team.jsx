737ace423aebf699027fb8f7e61011c34cfcc36e
ajhits <ajderoque@gmail.com>
2023-10-04 09:49:10 +0800

    pepe

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,8 +7,7 @@ export const Team = (props) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Welcome to our technical team. We are a group of dedicated professionals with a shared mission
           </p>
         </div>
         <div id="row">
