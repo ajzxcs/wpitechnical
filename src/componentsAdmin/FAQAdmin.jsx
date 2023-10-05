9286d8fd19558482afe61c5633bc626db5764ab4
WDFrancisBryan <143153810+WDFrancisBryan@users.noreply.github.com>
2023-10-05 23:27:32 +0800

    Add files via upload

---
+++ src/componentsAdmin/FAQAdmin.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "../assets/FAQ.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faArrowLeft } from "@fortawesome/free-solid-svg-icons"; // Import the back arrow icon
+import { FaqData, FaqData2 } from "../data/FaqData";
+import "../assets/PostDetail.css";
+
+const FAQ = ({ toggleFAQVisibility }) => {
+  const [faqs1, setFaqs1] = useState(FaqData);
+  const [faqs2, setFaqs2] = useState(FaqData2);
+
+  const toggleFAQ = (index, listName) => {
+    let updatedFaqs;
+
+    if (listName === "faqs1") {
+      updatedFaqs = faqs1.map((faq, i) => {
+        if (i === index) {
+          return { ...faq, isOpen: !faq.isOpen };
+        }
+        return { ...faq, isOpen: false };
+      });
+      setFaqs1(updatedFaqs);
+    } else if (listName === "faqs2") {
+      updatedFaqs = faqs2.map((faq, i) => {
+        if (i === index) {
+          return { ...faq, isOpen: !faq.isOpen };
+        }
+        return { ...faq, isOpen: false };
+      });
+      setFaqs2(updatedFaqs);
+    }
+  };
+
+  return (
+    <div className="faq-title">
+      <div className="faq-container">
+        <h2>Frequently Asked Questions</h2>
+        <div className="back-button">
+          <button onClick={toggleFAQVisibility}>
+            <FontAwesomeIcon icon={faArrowLeft} /> Back
+          </button>
+        </div>
+        <div className="faq-list">
+          {faqs1.map((faq, index) => (
+            <div key={index} className={`faq-item ${faq.isOpen ? "open" : ""}`}>
+              <div className="faq-content">
+                <div
+                  className="faq-question"
+                  onClick={() => toggleFAQ(index, "faqs1")}
+                >
+                  {faq.question}
+                </div>
+                <FontAwesomeIcon
+                  icon={faAngleDown}
+                  onClick={() => toggleFAQ(index, "faqs1")}
+                  className={`arrow ${faq.isOpen ? "open" : ""}`}
+                />
+              </div>
+              {faq.isOpen && <div className="faq-answer">{faq.answer}</div>}
+            </div>
+          ))}
+        </div>
+        <div className="faq-list">
+          {faqs2.map((faq, index) => (
+            <div key={index} className={`faq-item ${faq.isOpen ? "open" : ""}`}>
+              <div className="faq-content">
+                <div
+                  className="faq-question"
+                  onClick={() => toggleFAQ(index, "faqs2")}
+                >
+                  {faq.question}
+                </div>
+                <FontAwesomeIcon
+                  icon={faAngleDown}
+                  onClick={() => toggleFAQ(index, "faqs2")}
+                  className={`arrow ${faq.isOpen ? "open" : ""}`}
+                />
+              </div>
+              {faq.isOpen && <div className="faq-answer">{faq.answer}</div>}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
---
+++ src/componentsAdmin/PostFormAdmin.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import Select from "react-select"; // Import react-select
+import "../assets/public.css";
+
+Modal.setAppElement("#root");
+
+const PostForm = ({ isOpen, onRequestClose, onAddPost }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+  const [tagInput, setTagInput] = useState("");
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [brandInput, setBrandInput] = useState("");
+  const [IspostEmpty, setIspostEmpty] = useState(false);
+
+  const handleAddTag = () => {
+    if (tagInput) {
+      const newTag = tagInput.trim();
+      if (!tags.includes(newTag)) {
+        const updatedTags = [...tags, newTag];
+        setTags(updatedTags);
+        setTagInput("");
+      }
+    }
+  
+    // Include selected brands as tags
+    const selectedBrandTags = selectedBrands.map((brand) => ` ${brand.label}`);
+    if (selectedBrandTags.length > 0) {
+      const updatedTags = [...tags, ...selectedBrandTags];
+      setTags(updatedTags);
+      setSelectedBrands([]); // Clear selected brands
+    }
+  };
+
+  const handleCreatePost = () => {
+    if (content.trim() !== "") {
+      const newTags = [...tags, ...selectedBrands.map((brand) => ` ${brand.label}`)];
+      const newPost = {
+        title,
+        content,
+        newTags: newTags.join(", "),
+      };
+
+      onAddPost(newPost);
+
+      setTitle("");
+      setContent("");
+      setTags([]);
+      setTagInput("");
+      setSelectedBrands([]);
+      setBrandInput("");
+      onRequestClose();
+    } else {
+      setIspostEmpty(true);
+    }
+  };
+
+  const brands = [
+    { value: "Brand A", label: "Brand A" },
+    { value: "Brand B", label: "Brand B" },
+    { value: "Brand C", label: "Brand C" },
+    // Add more brands as needed
+  ];
+
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} className="post-form-modal">
+      <div className="post-form">
+        <div className="close-button" onClick={onRequestClose}>
+          <FontAwesomeIcon icon={faTimes} />
+        </div>
+        <h2>Create a New Post</h2>
+        <div className="post-form-details">
+          <p>{new Date().toLocaleString()}</p>
+          <input
+            type="text"
+            placeholder="Topic"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Concern"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className={IspostEmpty ? "error" : ""}
+          />
+          {IspostEmpty && (
+            <div className="alert-text">Content cannot be empty!</div>
+          )}
+
+          <br />
+          <br />
+          {/* Tags */}
+          {tags.length > 0 && (
+            <div className="selected-tags">
+              <strong>Selected Tags:</strong>{" "}
+              {tags.map((tag, index) => (
+                <span key={index} className="tag">
+                  {tag}
+                </span>
+              ))}
+            </div>
+          )}
+
+          {/* Brand selection */}
+          <div className="brand-selection">
+            <strong>Select Brands:</strong>
+            <Select
+              isMulti
+              name="brands"
+              options={brands}
+              value={selectedBrands}
+              onChange={(selectedOptions) => setSelectedBrands(selectedOptions)}
+            />
+          </div>
+
+          <br />
+          <div className="tag-input">
+            <input
+              type="text"
+              placeholder="Type here your specific Tags "
+              value={tagInput}
+              onChange={(e) => setTagInput(e.target.value)}
+            />
+            <button onClick={handleAddTag}>Add Tag</button>
+             <h6>To include tags, simply select the 'Add Tag' option.</h6>
+          </div>
+        </div>
+        <br />
+        <div style={{ textAlign: 'right' }}>
+      <button onClick={handleCreatePost}>Create Post</button>
+    </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default PostForm;
