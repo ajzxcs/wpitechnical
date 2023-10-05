fbbd110e5719e82099af2937efa4d3ed5fb62bee
ajhits <ajderoque@gmail.com>
2023-10-05 10:55:11 +0800

    hh

--- src/layouts/team/index.js
+++ src/layouts/team/index.js
@@ -5,16 +5,14 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import Footer from "examples/Footer";
 import "./Notif.css"; // You can create a CSS file for custom styling
 
-function Notifications() {
+function Team() {
   // Create state variables for input values
-  const [input1, setInput1] = useState({ name: "", content: "", image: null });
-  const [input2, setInput2] = useState({ name: "", content: "", image: null });
-  const [input3, setInput3] = useState({ name: "", content: "", image: null });
-  const [input4, setInput4] = useState({ name: "", content: "", image: null });
-  const [input5, setInput5] = useState({ name: "", content: "", image: null });
-  const [input6, setInput6] = useState({ name: "", content: "", image: null });
+  const [input1, setInput1] = useState({ name: "", position: "", image: null });
+  const [input2, setInput2] = useState({ name: "", position: "", image: null });
+  const [input3, setInput3] = useState({ name: "", position: "", image: null });
+  const [input4, setInput4] = useState({ name: "", position: "", image: null });
 
-  // Function to handle input changes for name and content
+  // Function to handle input changes for name and position
   const handleInputChange = (input, event) => {
     const { name, value } = event.target;
     input({ ...input, [name]: value });
@@ -29,24 +27,17 @@ function Notifications() {
   // Function to handle button click for each input set
   const handleUpload = (input) => {
     console.log("Name:", input.name);
-    console.log("Content:", input.content);
+    console.log("Position:", input.position);
     console.log("Image File:", input.image);
   };
 
   return (
     <DashboardLayout>
-    <h1 style={{ marginBottom: "20px" }}>Forum</h1> {/* Forum Header */}
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
-        style={{ minHeight: "calc(100vh - 100px)" }}
-      >
-      
+      <h1 style={{ marginBottom: "20px" }}>Teams</h1> {/* Forum Header */}
+      <div className="input-grid">
         {/* Input Set 1 */}
         <div className="input-set">
-          <p>Display 1</p>
+          <p>Person 1</p>
           <input
             type="text"
             name="name"
@@ -56,9 +47,9 @@ function Notifications() {
           />
           <input
             type="text"
-            name="content"
-            placeholder="Content"
-            value={input1.content}
+            name="position"
+            placeholder="Position"
+            value={input1.position}
             onChange={(e) => handleInputChange(input1, e)}
           />
           <input
@@ -78,7 +69,7 @@ function Notifications() {
 
         {/* Input Set 2 */}
         <div className="input-set">
-          <p>Display 2</p>
+          <p>Person 2</p>
           <input
             type="text"
             name="name"
@@ -88,9 +79,9 @@ function Notifications() {
           />
           <input
             type="text"
-            name="content"
-            placeholder="Content"
-            value={input2.content}
+            name="position"
+            placeholder="Position"
+            value={input2.position}
             onChange={(e) => handleInputChange(input2, e)}
           />
           <input
@@ -110,7 +101,7 @@ function Notifications() {
 
         {/* Input Set 3 */}
         <div className="input-set">
-          <p>Display 3</p>
+          <p>Person 3</p>
           <input
             type="text"
             name="name"
@@ -120,9 +111,9 @@ function Notifications() {
           />
           <input
             type="text"
-            name="content"
-            placeholder="Content"
-            value={input3.content}
+            name="position"
+            placeholder="Position"
+            value={input3.position}
             onChange={(e) => handleInputChange(input3, e)}
           />
           <input
@@ -142,7 +133,7 @@ function Notifications() {
 
         {/* Input Set 4 */}
         <div className="input-set">
-          <p>Display 4</p>
+          <p>Person 4</p>
           <input
             type="text"
             name="name"
@@ -152,9 +143,9 @@ function Notifications() {
           />
           <input
             type="text"
-            name="content"
-            placeholder="Content"
-            value={input4.content}
+            name="position"
+            placeholder="Position"
+            value={input4.position}
             onChange={(e) => handleInputChange(input4, e)}
           />
           <input
@@ -171,82 +162,11 @@ function Notifications() {
             Upload
           </MDButton>
         </div>
+      </div>
 
-        {/* Input Set 5 */}
-        <div className="input-set">
-          <p>Display 5</p>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={input5.name}
-            onChange={(e) => handleInputChange(input5, e)}
-          />
-          <input
-            type="text"
-            name="content"
-            placeholder="Content"
-            value={input5.content}
-            onChange={(e) => handleInputChange(input5, e)}
-          />
-          <input
-            type="file"
-            name="image"
-            accept="image/*"
-            onChange={(e) => handleFileChange(input5, e)}
-          />
-          <MDButton
-            variant="contained"
-            color="primary"
-            onClick={() => handleUpload(input5)}
-          >
-            Upload
-          </MDButton>
-        </div>
-
-        {/* Input Set 6 */}
-        <div className="input-set">
-          <p>Display 6</p>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={input6.name}
-            onChange={(e) => handleInputChange(input6, e)}
-          />
-          <input
-            type="text"
-            name="content"
-            placeholder="Content"
-            value={input6.content}
-            onChange={(e) => handleInputChange(input6, e)}
-          />
-          <input
-            type="file"
-            name="image"
-            accept="image/*"
-            onChange={(e) => handleFileChange(input6, e)}
-          />
-          <MDButton
-            variant="contained"
-            color="primary"
-            onClick={() => handleUpload(input6)}
-          >
-            Upload
-          </MDButton>
-        </div>
-
-        <MDButton
-          variant="contained"
-          color="warning"
-          href="index.html" // Link to the index.html file
-        >
-          Login as SuperAdmin
-        </MDButton>
-        <Footer />
-      </Grid>
+      <Footer />
     </DashboardLayout>
   );
 }
 
-export default Notifications;
+export default Team;
