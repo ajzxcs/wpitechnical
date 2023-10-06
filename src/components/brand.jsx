51b7d58b4bf212b2d5268a2c778a4017f6d79c53
ajhits <ajderoque@gmail.com>
2023-10-06 16:27:28 +0800

    lele

--- src/components/brand.jsx
+++ src/components/brand.jsx
@@ -49,7 +49,6 @@ export const Brand = (props) => {
     <div id="brand" className="text-center">
       <div className="container">
         <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Brands</h2>
         </div>
         <div className="row">
           {brands ? (
