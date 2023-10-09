76b7fe1720b50dadec2fb10ef3ef6818545667a3
ajhits <ajderoque@gmail.com>
2023-10-09 12:41:33 +0800

    gg

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import AccountBox from "./component/accountBox/AccountBox";
+import './index.css';
+
+const AppContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Login = () => {
   return (
-    <div>HOY TANGINA MO AJ DITO KA MAG LAGAY NG LOGIN FORM</div>
+    <AppContainer>
+    <AccountBox />
+    </AppContainer>
   )
 }
 
---
+++ src/component/accountBox/loginForm.jsx
@@ -0,0 +1,108 @@
+import React, { useContext, useState } from "react";
+import {
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+  BoldLink
+} from "./common";
+import { Marginer } from "../marginer";
+
+function LoginForm() {
+  const [showPasswordCheckbox, setShowPasswordCheckbox] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleShowPasswordCheckbox = () => {
+    setShowPasswordCheckbox(
+      (prevShowPasswordCheckbox) => !prevShowPasswordCheckbox
+    );
+    setShowPassword(false); // Hide the password when toggling the checkbox
+  };
+
+  const handleEmailChange = (e) => {
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+
+    // Basic email validation using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsEmailValid(emailRegex.test(emailValue));
+  };
+
+  const handleSubmit = () => {
+    if (!isEmailValid) {
+      // Handle invalid email here (e.g., display an error message)
+      console.log("Invalid email");
+      return;
+    }
+
+    // Handle form submission with valid email and password
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div style={{ margin: "0 auto", width: "100%"}}>
+    <BoxContainer>
+    <FormContainer>
+      <Marginer direction="vertical" />
+      <Input
+        type="text"
+        placeholder="Email"
+        required
+        onChange={handleEmailChange}
+        value={email}
+        style={!isEmailValid ? { border: "1px solid red" } : {}}
+      />
+      {!isEmailValid && (
+        <div style={{ color: "red", fontSize: "13px", marginTop: "5px" }}>
+          Invalid email address
+        </div>
+      )}
+      <Marginer direction="vertical" />
+      <Input
+        type={showPasswordCheckbox ? "text" : "password"}
+        placeholder="Password"
+        required
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: "20px",
+          marginLeft: "3px",
+          fontSize: "13px",
+          color: "#666"
+        }}
+      >
+        <input
+          type="checkbox"
+          checked={showPasswordCheckbox}
+          onChange={handleShowPasswordCheckbox}
+          style={{ marginRight: "6px" }}
+        />
+        <span>Show Password</span>
+      </div>
+      <Marginer direction="horizontal" margin="10px" />
+      <MutedLink href="#">Forgot Your Password?</MutedLink>
+      <Marginer direction="vertical" margin="10px" />
+      <SubmitButton type="button" onClick={handleSubmit}>
+        Sign in
+      </SubmitButton>
+      <Marginer direction="vertical" margin="10px" />
+    </FormContainer>
+  </BoxContainer>
+</div>
+);
+}
+
+export default LoginForm;
